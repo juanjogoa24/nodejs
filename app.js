5b8692425afae3215d27fc032076a9161eaffe14
Juanjogoa <juanjogoa24@gmail.com>
2020-06-11 11:22:11 -0500

    modificacion

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-const fs = require('fs');
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
 
-let base = 3;
-let data = '';
-for (let i = 1; i <= 10; i++) {
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
-    data += (`${base} * ${i} = ${base * i} \n`);
+let comando = argv._[0];
+
+
+switch (comando) {
+
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: `, colors.green(archivo)))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
 
 }
 
-//const data = new Uint8Array(Buffer.from('Hello Node.js'));
-fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-});
+
+
+// console.log(argv.base);
+
+
+
+// let parametro = argv[2];
+// let base = parametro.split('=')[1]
